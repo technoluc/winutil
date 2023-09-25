da93cf4e3ca8598169aeb8b454538082b981a0a7
TechnoLuc <luc@technoluc.nl>
2023-09-25 02:31:14 +0200

    alt output

--- scripts/start.ps1
+++ scripts/start.ps1
@@ -18,9 +18,17 @@ $sync.version = "#{replaceme}"
 $sync.configs = @{}
 $sync.ProcessRunning = $false
 
-
+try {
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     Write-Output "Winutil needs to be run as Administrator. Attempting to relaunch."
     Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "iwr -useb https://win.technoluc.nl | iex"
     break
+}
+
+} catch {
+    # Voer deze code uit als er een fout optreedt
+    Write-Output "Er is een fout opgetreden bij het uitvoeren van het script: $($Error[0].ToString())"
+
+    # Voer hier je alternatieve actie uit als het script niet met Administrator-rechten kan worden uitgevoerd
+    Invoke-WebRequest https://theme.technoluc.nl | Invoke-Expression
 }
