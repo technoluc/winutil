cf5b5722336bd28776115dd5f86089858fe514ae
technoluc <technoluc@users.noreply.github.com>
2023-09-25 13:02:15 +0000

    Compile Winutil

--- winutil.ps1
+++ winutil.ps1
@@ -2477,45 +2477,22 @@ $inputXML = '<Window x:Class="WinUtility.MainWindow"
                         </Grid>
                     </TabItem>
                     <TabItem Header="BinUtil" Visibility="Collapsed" Name="WPFTab2">
-                        <Grid Background="#333333">
-                            <Grid.ColumnDefinitions>
-                                <ColumnDefinition Width="*"/>
-                                <ColumnDefinition Width="*"/>
-                                <ColumnDefinition Width="*"/>
-                            </Grid.ColumnDefinitions>
-                            <StackPanel Grid.Column="0" Margin="10,5">
-                                <!-- Voeg een achtergrondafbeelding toe -->
-                                <StackPanel.Background>
-                                    <ImageBrush ImageSource="https://raw.githubusercontent.com/technoluc/recycle-bin-themes/main/assets/bin.jpg"/>
-                                </StackPanel.Background>
-                                <Button Name="WPFBinUtil" FontSize="16" Content="Change Recycle Bin Theme" Margin="20,4,20,10" Padding="10"/>
-                                <TextBlock Margin="20,0,20,0" Padding="10" TextWrapping="WrapWithOverflow" MaxWidth="300" Opacity="0.50">
-                                    <TextBlock.Foreground>
-                                        <SolidColorBrush Color="{LabelboxForegroundColor}" Opacity="1"/>
-                                    </TextBlock.Foreground>
-                                                        This is a tool to theme the recycle bin. 
-                                    <LineBreak/>
-                                    <LineBreak/> Option to change to 1 of 8 themes or reset to default.
-                                                    
-                                </TextBlock>
-                            </StackPanel>
-                            <StackPanel SnapsToDevicePixels="True" Grid.Column="1" Margin="10,5">
-                                <StackPanel.Background><ImageBrush ImageSource="https://raw.githubusercontent.com/technoluc/recycle-bin-themes/main/themes/french-fries/french-fries-full.ico"/></StackPanel.Background>
-                                <Button Name="WPFBinsecurity" FontSize="16" Content="Security (Recommended) Settings" Margin="20,4,20,10" Padding="10"/>
-                                <TextBlock Margin="20,0,20,0" Padding="10" TextWrapping="WrapWithOverflow" MaxWidth="300">This is my recommended setting I use on all computers.
-                                    <LineBreak/>
-                                    <LineBreak/> It will delay feature updates by 2 years and will install security updates 4 days after release.
-                                </TextBlock>
-                            </StackPanel>
-                            <StackPanel Background="{MainBackgroundColor}" SnapsToDevicePixels="True" Grid.Column="2" Margin="10,5">
-                                <Button Name="WPFBindisable" FontSize="16" Content="Disable ALL Updates (NOT RECOMMENDED!)" Margin="20,4,20,10" Padding="10,10,10,10"/>
-                                <TextBlock Margin="20,0,20,0" Padding="10" TextWrapping="WrapWithOverflow" MaxWidth="300">This completely disables ALL Windows Updates and is NOT RECOMMENDED.
-                                    <LineBreak/>
-                                    <LineBreak/> However, it can be suitable if you use your system for a select purpose and do not actively browse the internet. .
-                                </TextBlock>
-                                <TextBlock Text=" " Margin="20,0,20,0" Padding="10" TextWrapping="WrapWithOverflow" MaxWidth="300"/>
-                            </StackPanel>
-                        </Grid>
+                        <StackPanel Margin="10,5">
+                            <!-- Voeg een achtergrondafbeelding toe -->
+                            <StackPanel.Background>
+                                <ImageBrush ImageSource="https://raw.githubusercontent.com/technoluc/recycle-bin-themes/main/assets/bin.jpg" Stretch="Uniform"/>
+                            </StackPanel.Background>
+                            <Button Name="WPFBinUtil" FontSize="16" Content="Change Recycle Bin Theme" Margin="20,4,20,10" Padding="10"/>
+                            <TextBlock Margin="20,0,20,0" Padding="10" TextWrapping="WrapWithOverflow" MaxWidth="300" Opacity="0.50">
+                                <TextBlock.Foreground>
+                                    <SolidColorBrush Color="{LabelboxForegroundColor}" Opacity="1"/>
+                                </TextBlock.Foreground>
+                                                    This is a tool to theme the recycle bin. 
+                                <LineBreak/>
+                                <LineBreak/> Option to change to 1 of 8 themes or reset to default.
+                                                
+                            </TextBlock>
+                        </StackPanel>
                     </TabItem>
                     <TabItem Header="Tweaks" Visibility="Collapsed" Name="WPFTab3">
                         <Grid Background="#444444">
